ce814bc9d668129c415cca742442053703c24c35
Sergio Cruz <vlad.crz@gmail.com>
2022-12-31 01:21:15 -0600

    Adding first commit

---
+++ src/main/java/com/infomedia/sf5recipeapp/Sf5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.infomedia.sf5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf5RecipeAppApplication.class, args);
+    }
+
+}
